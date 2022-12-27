bb91c4ef23d9fafc1cf47fdc4b0e0456498f8a29
Andrea1904 <alopezm@bancodeoccidente.com.co>
2022-12-27 00:56:34 -0500

    Add files via upload

--- objetos.js
+++ objetos.js
@@ -23,3 +23,25 @@ auto();
 var autoNuevo = new auto("tesla","modelo",2021);
 
 console.log(autoNuevo);
+
+
+// crear los 30 carros de manera automatica 
+
+function misAutos(marca, modelo, annio){
+    this.marca=marca;
+    this.modelo=modelo;
+    this.annio=annio;
+    
+}
+
+let autos = [];
+  for(let i = 0 ; i < 30 ; i++){
+    var marca_1 = ("Patito"+i);
+    var modelo_1= ("Modelo: "+i);
+    var annio_1 = ("Año: "+i);
+    autos.push(new auto (marca_1, modelo_1, annio_1));
+  }
+  
+  for(let i = 0 ; i < autos.length ; i++){
+    console.log(autos[i]);
+  }
